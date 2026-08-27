ec74ef09c9f9868f3c031e488fec42d92c1ed735
Travline <poluxmorales1@gmail.com>
2026-08-27 15:03:54 -0500

    using navigation via Link instead of onClick event

--- apps/cms/src/pages/not-found.tsx
+++ apps/cms/src/pages/not-found.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 export const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-3 min-h-screen bg-background text-foreground">
       <h1 className="text-4xl font-bold">404</h1>
       <p className="text-lg">Página no encontrada</p>
-      <Button onClick={() => window.location.href = "/"} variant="default" size="lg" className="">
-        Volver a la página principal
+      <Button variant="default" size="lg" className="">
+        <Link to="/">Volver a la página principal</Link>
       </Button>
     </div>
   );
